7744631e2e862a0db68e6582f104e48cf7854b36
Momin Ahmad <mominr082@gmail.com>
2024-07-14 17:20:55 +0530

    Update README and pubspec.yaml

--- example/base64_url_safe_uuid_example.dart
+++ example/base64_url_safe_uuid_example.dart
@@ -1,5 +1,7 @@
 import 'package:base64_url_safe_uuid/base64_url_safe_uuid.dart';
 
 void main() {
-  print('base64 url safe uuid: ${UrlSafeUuid.generate()}');
+  final base64UrlSafeUuid = UrlSafeUuid.generate();
+
+  print('base64 url safe uuid: $base64UrlSafeUuid');
 }
