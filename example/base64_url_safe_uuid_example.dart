8789fe0cbd94a05a29c26f63ff8991c4aff15276
Momin Ahmad <mominr082@gmail.com>
2024-07-14 15:20:02 +0530

    first commit

---
+++ example/base64_url_safe_uuid_example.dart
@@ -0,0 +1,6 @@
+import 'package:base64_url_safe_uuid/base64_url_safe_uuid.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/base64_url_safe_uuid_test.dart
@@ -0,0 +1,16 @@
+import 'package:base64_url_safe_uuid/base64_url_safe_uuid.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
